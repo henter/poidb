4690f1937cee3dd06534f0c8790eccd72bc66475
henter <henter@henter.me>
2019-06-10 20:24:50 +0800

    up

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/henter/poidb"
 	"github.com/rs/xid"
@@ -8,11 +9,23 @@ import (
 	"github.com/tidwall/geojson/geo"
 	"github.com/tidwall/geojson/geometry"
 	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"runtime"
+	"runtime/debug"
 	"sort"
 	"syscall"
 	"time"
+)
+
+var (
+	max    int
+	dbPath string
+	pprof  string
+	gogc int
+	gcs int
 )
 
 type Item struct {
@@ -21,9 +34,26 @@ type Item struct {
 }
 
 func main() {
+	flag.IntVar(&max, "max", 1000000, "max poi to test")
+	flag.StringVar(&pprof, "pprof", ":9901", "pprof listen")
+	flag.StringVar(&dbPath, "path", "/tmp/poi.db", "db file path")
+	flag.IntVar(&gogc, "gogc", 20, "go gc")
+	flag.IntVar(&gcs, "gcs", 20, "gc second")
+	flag.Parse()
+
+	go func() {
+		err := http.ListenAndServe(pprof, nil)
+		if err != nil {
+			fmt.Printf("pprof listen error %s", err)
+		}
+	}()
+
+	go watchOutOfMemory()
+
+	fmt.Printf("start db %s\n", dbPath)
 	//db, err := poidb.Open(":memory:")
 	t := time.Now()
-	db, err := poidb.Open("/tmp/poi.db")
+	db, err := poidb.Open(dbPath)
 	if err != nil {
 		fmt.Println("err " + err.Error())
 		return
@@ -64,7 +94,6 @@ func main() {
 		return
 	}
 
-	max := 80000000
 	i := db.Count()
 	rand.Seed(time.Now().UnixNano())
 
@@ -81,26 +110,56 @@ func main() {
 			if err != nil {
 				fmt.Println("err " + err.Error())
 			}
+			if i%10000 == 0 {
+				fmt.Printf("i %d \n", i)
+			}
 		}
 	}
 	fmt.Printf("db count %d\n", db.Count())
 	fmt.Println("done")
 
 	//query bench
-	max = 100000
-	i = 0
+	max = 1000000
 	var items []Item
-	t2 := time.Now()
-	for i < max {
-		i++
-		lng = 90.123 + float64(rand.Intn(40)) + rand.Float64()
-		lat = 18.2423 + float64(rand.Intn(30)) + rand.Float64()
+	var c int
+	for {
+		i = 0
+		c = 0
+		t2 := time.Now()
+		for i < max{
+			i++
+			lng = 90.123 + float64(rand.Intn(40)) + rand.Float64()
+			lat = 18.2423 + float64(rand.Intn(30)) + rand.Float64()
 
-		items = Nearby(db, lng, lat, 500, 10)
-		fmt.Printf("near items %d\n", len(items))
+			items = Nearby(db, lng, lat, 500, 10)
+			//fmt.Printf("near items %d\n", len(items))
+			c += len(items)
+		}
+		fmt.Printf("query %d times in %s, total items %d\n", max, time.Since(t2), c)
 	}
-	fmt.Printf("query %d times in %s\n", max, time.Since(t2))
-	select {}
+
+}
+
+func watchOutOfMemory() {
+	if gogc != -1 {
+		debug.SetGCPercent(gogc)
+		return
+	}
+
+	debug.SetGCPercent(-1)
+	t := time.NewTicker(time.Second * time.Duration(gcs))
+	defer t.Stop()
+	//var mem runtime.MemStats
+	//var oom = false
+	for range t.C {
+		runtime.GC()
+		//runtime.ReadMemStats(&mem)
+		//oom = int(mem.HeapAlloc) > 2 * 1024 * 1024 * 1024
+		//if oom {
+		//	runtime.GC()
+		//	log.Info("watch oom gc", zap.Any("healAlloc", mem.HeapAlloc))
+		//}
+	}
 }
 
 func Nearby(db *poidb.DB, lng, lat float64, meters float64, limit int) (items []Item) {
